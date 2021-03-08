5841f27f366e59108f2bb9883dec7b4848cabc7a
youyiqin <564151947@qq.com>
2021-03-08 20:38:18 +0800

    add: set

--- src/directory/index.ts
+++ src/directory/index.ts
@@ -1,7 +1,28 @@
+import { defaultToString } from "../help";
+
 export default class Directory {
   table: Object;
 
+  defaultToString: (item: any) => string; // eslint-disable-line
+
   constructor() {
     this.table = {};
+    this.defaultToString = defaultToString;
+  }
+
+  set(key: unknown, value: unknown) {
+    this.table[this.defaultToString(key)] = value;
+  }
+
+  remove(key: unknown): boolean {
+    if (this.has(key)) {
+      delete this.table[this.defaultToString(key)];
+      return true;
+    }
+    return false;
+  }
+
+  has(key: unknown): boolean {
+    return Object.prototype.hasOwnProperty.call(this.table, [key]);
   }
 }
--- src/help.ts
+++ src/help.ts
@@ -3,3 +3,17 @@ export const isLetter = (str: string) => /[a-z]/.test(str);
 export const defaultEquals = (a: number | string, b: number | string) => {
   return a === b;
 };
+
+export const defaultToString = (item: any): string => {
+  if (item === null) {
+    return "NULL";
+  }
+  if (item === undefined) {
+    return "UNDEFINED";
+  }
+  if (typeof item === "string" || item instanceof String) {
+    return `${item}`;
+  }
+  const res: string = item.toString();
+  return res;
+};
